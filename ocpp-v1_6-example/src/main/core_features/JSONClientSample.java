9c6db739a9dd93b433b8d7592731b35846d265ff
Thomas Volden <vasevolden@gmail.com>
2016-06-14 20:18:29 +0200

    Created an example of the JSON Client

---
+++ ocpp-v1_6-example/src/main/core_features/JSONClientSample.java
@@ -0,0 +1,141 @@
+package core_features;
+
+import eu.chargetime.ocpp.Client;
+import eu.chargetime.ocpp.JSONClient;
+import eu.chargetime.ocpp.feature.profile.ClientCoreEventHandler;
+import eu.chargetime.ocpp.feature.profile.CoreProfile;
+import eu.chargetime.ocpp.model.*;
+
+/**
+ * ChargeTime.eu - Java-OCA-OCPP
+ * Copyright (C) 2015-2016 Thomas Volden <tv@chargetime.eu>
+ * <p>
+ * MIT License
+ * <p>
+ * Copyright (c) 2016 Thomas Volden
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+public class JSONClientSample {
+    private Client client;
+    private CoreProfile core;
+
+    public void connect() throws Exception {
+
+        // The core profile is mandatory
+        core = new CoreProfile(new ClientCoreEventHandler() {
+            @Override
+            public ChangeAvailabilityConfirmation handleChangeAvailabilityRequest(ChangeAvailabilityRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public GetConfigurationConfirmation handleGetConfigurationRequest(GetConfigurationRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public ChangeConfigurationConfirmation handleChangeConfigurationRequest(ChangeConfigurationRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public ClearCacheConfirmation handleClearCacheRequest(ClearCacheRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public DataTransferConfirmation handleDataTransferRequest(DataTransferRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public RemoteStartTransactionConfirmation handleRemoteStartTransactionRequest(RemoteStartTransactionRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public RemoteStopTransactionConfirmation handleRemoteStopTransactionRequest(RemoteStopTransactionRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public ResetConfirmation handleResetRequest(ResetRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public UnlockConnectorConfirmation handleUnlockConnectorRequest(UnlockConnectorRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+        });
+        client = new JSONClient(core);
+        client.connect("ws://hostname:8887");
+    }
+
+    public void sendBootNotification() throws Exception {
+
+        // Use the feature profile to help create event
+        Request request = core.createBootNotificationRequest("some vendor", "some model");
+
+        // Client returns a promise which will be filled once it receives a confirmation.
+        client.send(request).whenComplete((s, ex) -> System.out.println(s));
+    }
+
+    public void disconnect() {
+        client.disconnect();
+    }
+
+}
