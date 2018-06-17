5bd437bc6806d79a479b9f83910f1b7437f11c0b
Spyridon Papadopoulos <spyridon.papadopoulos@ubitricity.com>
2018-06-17 22:27:17 +0200

    Adapt localauthlist and relevant tests.

--- ocpp-v1_6/src/main/java/eu/chargetime/ocpp/model/localauthlist/SendLocalListRequest.java
+++ ocpp-v1_6/src/main/java/eu/chargetime/ocpp/model/localauthlist/SendLocalListRequest.java
@@ -5,59 +5,60 @@ import eu.chargetime.ocpp.model.Request;
 
 public class SendLocalListRequest implements Request {
 
-	private int listVersion = 0;
-	private AuthorizationData [] localAuthorizationList = null;
-	private UpdateType updateType = null;
+    private int listVersion = 0;
+    private AuthorizationData[] localAuthorizationList = null;
+    private UpdateType updateType = null;
 
-    public SendLocalListRequest() { }
+    public SendLocalListRequest() {
+    }
 
     public SendLocalListRequest(int listVersion, UpdateType updateType) {
         this.listVersion = listVersion;
         this.updateType = updateType;
     }
 
-    public void setListVersion(int listVersion) throws PropertyConstraintException {
-		if(listVersion < 1) throw new PropertyConstraintException("listVersion", listVersion, "Should be greater than 0");
-		this.listVersion = listVersion;
-	}
-	
-	public int getListVersion() {
-		return listVersion;
-	}
-	
-	public void setLocalAuthorizationList(AuthorizationData [] localAuthorizationList) {
-		this.localAuthorizationList = localAuthorizationList;
-	}
-	
-	public AuthorizationData [] getLocalAuthorizationList() {
-		return localAuthorizationList;
-	}
-	
-	public void setUpdateType(UpdateType updateType) {
-		this.updateType = updateType;
-	}
-	
-	public UpdateType getUpdateType() {
-		return updateType;
-	}
-	
-	@Override
-	public boolean validate() {
-		boolean valid = true;
-		
-		if(localAuthorizationList != null) {
-			for(AuthorizationData data : localAuthorizationList) {
-				valid &= data.validate();
-			}
-		}
-		
-		return valid
-				&& (listVersion >= 1)
-				&& (updateType != null);
-	}
+    public void setListVersion(int listVersion) {
+        if (listVersion < 1) {
+            throw new PropertyConstraintException(listVersion, "listVersion must be > 0");
+        }
+        this.listVersion = listVersion;
+    }
 
-	@Override
-	public boolean transactionRelated() {
-		return false;
-	}
+    public int getListVersion() {
+        return listVersion;
+    }
+
+    public void setLocalAuthorizationList(AuthorizationData[] localAuthorizationList) {
+        this.localAuthorizationList = localAuthorizationList;
+    }
+
+    public AuthorizationData[] getLocalAuthorizationList() {
+        return localAuthorizationList;
+    }
+
+    public void setUpdateType(UpdateType updateType) {
+        this.updateType = updateType;
+    }
+
+    public UpdateType getUpdateType() {
+        return updateType;
+    }
+
+    @Override
+    public boolean validate() {
+        boolean valid = (listVersion >= 1) && (updateType != null);
+
+        if (localAuthorizationList != null) {
+            for (AuthorizationData data : localAuthorizationList) {
+                valid &= data.validate();
+            }
+        }
+
+        return valid;
+    }
+
+    @Override
+    public boolean transactionRelated() {
+        return false;
+    }
 }
