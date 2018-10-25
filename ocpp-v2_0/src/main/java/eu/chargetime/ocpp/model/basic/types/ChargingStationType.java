705cf804c889a97aea7b60d3a17675f5a60aeef4
Thomas Volden <tv@chargetime.eu>
2018-10-25 22:37:46 +0200

    Added v2_0

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/basic/types/ChargingStationType.java
@@ -0,0 +1,81 @@
+package eu.chargetime.ocpp.model.basic.types;
+/*
+    ChargeTime.eu - Java-OCA-OCPP
+    
+    MIT License
+
+    Copyright (C) 2018 Thomas Volden <tv@chargetime.eu>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+ */
+
+import eu.chargetime.ocpp.model.Validatable;
+
+public class ChargingStationType implements Validatable {
+    private String serialNumber;
+    private String model;
+    private String vendorName;
+    private String firmwareVersion;
+    private ModemType modem;
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getFirmwareVersion() {
+        return firmwareVersion;
+    }
+
+    public void setFirmwareVersion(String firmwareVersion) {
+        this.firmwareVersion = firmwareVersion;
+    }
+
+    public ModemType getModem() {
+        return modem;
+    }
+
+    public void setModem(ModemType modem) {
+        this.modem = modem;
+    }
+
+    @Override
+    public boolean validate() {
+        return false;
+    }
+}
