debde0c892c4cf1ebbdd81c79889a50ed003397f
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-13 18:44:02 +0200

    RegistrationStatusEnumType.java was refactored

--- ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/types/RegistrationStatusEnumType.java
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/types/RegistrationStatusEnumType.java
@@ -25,19 +25,49 @@ package eu.chargetime.ocpp.model.types;
    SOFTWARE.
 */
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
 public enum RegistrationStatusEnumType {
   /** Charging Station is accepted by the CSMS. */
-  Accepted,
+  ACCEPTED("Accepted"),
 
   /**
    * CSMS is not yet ready to accept the Charging Station. CSMS may send messages to retrieve
    * information or prepare the Charging Station.
    */
-  Pending,
+  PENDING("Pending"),
 
   /**
    * Charging Station is not accepted by CSMS. This may happen when the Charging Station id is not
    * known by CSMS.
    */
-  Rejected
+  REJECTED("Rejected");
+
+  private final String value;
+
+  RegistrationStatusEnumType(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return this.value;
+  }
+
+  @JsonValue
+  public String value() {
+    return this.value;
+  }
+
+  @JsonCreator
+  public static RegistrationStatusEnumType fromValue(String value) {
+    return findByField(
+            RegistrationStatusEnumType.class,
+            RegistrationStatusEnumType::value,
+            value
+    );
+  }
 }
