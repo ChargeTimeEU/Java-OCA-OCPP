7f9b3f219fa0d212319b514f61b1a3876751f1ed
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-15 00:02:59 +0200

    added IdTokenEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/IdTokenEnumType.java
@@ -0,0 +1,48 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+/**
+ * Enumeration of possible idToken types.
+ *
+ *
+ */
+public enum IdTokenEnumType {
+
+    CENTRAL("Central"),
+    E_MAID("eMAID"),
+    ISO_14443("ISO14443"),
+    ISO_15693("ISO15693"),
+    KEY_CODE("KeyCode"),
+    LOCAL("Local"),
+    MAC_ADDRESS("MacAddress"),
+    NO_AUTHORIZATION("NoAuthorization");
+    private final String value;
+
+    IdTokenEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static IdTokenEnumType fromValue(String value) {
+        return findByField(
+                IdTokenEnumType.class,
+                IdTokenEnumType::value,
+                value
+        );
+    }
+
+}
