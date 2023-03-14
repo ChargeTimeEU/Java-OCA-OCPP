2e7cc5e973ac75e7ebd31de55667d74765cf51f9
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-15 00:02:25 +0200

    added ChargingProfileKindEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/ChargingProfileKindEnumType.java
@@ -0,0 +1,45 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+/**
+ * Charging_ Profile. Charging_ Profile_ Kind. Charging_ Profile_ Kind_ Code
+ * urn:x-oca:ocpp:uid:1:569232
+ * Indicates the kind of schedule.
+ *
+ *
+ */
+public enum ChargingProfileKindEnumType {
+
+    ABSOLUTE("Absolute"),
+    RECURRING("Recurring"),
+    RELATIVE("Relative");
+    private final String value;
+
+    ChargingProfileKindEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static ChargingProfileKindEnumType fromValue(String value) {
+        return findByField(
+                ChargingProfileKindEnumType.class,
+                ChargingProfileKindEnumType::value,
+                value
+        );
+    }
+
+}
