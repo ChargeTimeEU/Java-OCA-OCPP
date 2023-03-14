999960336d2aec32d6d6acc85aec9c2e4bb312ef
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-15 00:02:53 +0200

    added CostKindEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/CostKindEnumType.java
@@ -0,0 +1,45 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+/**
+ * Cost. Cost_ Kind. Cost_ Kind_ Code
+ * urn:x-oca:ocpp:uid:1:569243
+ * The kind of cost referred to in the message element amount
+ *
+ *
+ */
+public enum CostKindEnumType {
+
+    CARBON_DIOXIDE_EMISSION("CarbonDioxideEmission"),
+    RELATIVE_PRICE_PERCENTAGE("RelativePricePercentage"),
+    RENEWABLE_GENERATION_PERCENTAGE("RenewableGenerationPercentage");
+    private final String value;
+
+    CostKindEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static CostKindEnumType fromValue(String value) {
+        return findByField(
+                CostKindEnumType.class,
+                CostKindEnumType::value,
+                value
+        );
+    }
+
+}
