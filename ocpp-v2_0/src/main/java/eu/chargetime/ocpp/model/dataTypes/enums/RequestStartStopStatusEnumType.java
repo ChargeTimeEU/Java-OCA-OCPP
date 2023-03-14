cd2b37f36c6cbd11316e967c5f7b0776f594c9df
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-15 00:04:02 +0200

    added RequestStartStopStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/RequestStartStopStatusEnumType.java
@@ -0,0 +1,42 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+/**
+ * Status indicating whether the Charging Station accepts the request to start a transaction.
+ *
+ *
+ */
+public enum RequestStartStopStatusEnumType {
+
+    ACCEPTED("Accepted"),
+    REJECTED("Rejected");
+    private final String value;
+
+    RequestStartStopStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static RequestStartStopStatusEnumType fromValue(String value) {
+        return findByField(
+                RequestStartStopStatusEnumType.class,
+                RequestStartStopStatusEnumType::value,
+                value
+        );
+    }
+
+}
