4cb238c6c0afe02a558b81c324ad2ca0b6761678
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-15 17:10:42 +0200

    added CustomData.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/CustomData.java
@@ -0,0 +1,36 @@
+package eu.chargetime.ocpp.model.dataTypes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+ *
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "vendorId"
+})
+@Getter
+@EqualsAndHashCode
+public class CustomData {
+
+    /**
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("vendorId")
+    public String vendorId;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<>();
+
+}
