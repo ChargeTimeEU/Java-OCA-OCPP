8c83028988f507033a4041324dc872a9ab117063
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-16 17:28:37 +0200

    added OperationalStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/OperationalStatusEnumType.java
@@ -0,0 +1,43 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * This contains the type of availability change that the Charging Station should perform.
+ *
+ *
+ *
+ */
+public enum OperationalStatusEnumType {
+
+    INOPERATIVE("Inoperative"),
+    OPERATIVE("Operative");
+    private final String value;
+
+    OperationalStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static OperationalStatusEnumType fromValue(String value) {
+        return findByField(
+                OperationalStatusEnumType.class,
+                OperationalStatusEnumType::value,
+                value
+        );
+    }
+}
