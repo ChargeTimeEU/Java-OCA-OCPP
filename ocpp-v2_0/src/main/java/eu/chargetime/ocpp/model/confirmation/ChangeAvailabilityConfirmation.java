a00cda620dbcd31797d58b26485446149aac9c24
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-16 17:40:49 +0200

    added ChangeAvailabilityConfirmation.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/confirmation/ChangeAvailabilityConfirmation.java
@@ -0,0 +1,66 @@
+package eu.chargetime.ocpp.model.confirmation;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.Confirmation;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.dataTypes.StatusInfoType;
+import eu.chargetime.ocpp.model.dataTypes.enums.ChangeAvailabilityStatusEnumType;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "status",
+        "statusInfo"
+})
+public class ChangeAvailabilityConfirmation extends Confirmation {
+
+    private transient RequiredValidator requiredValidator = new RequiredValidator();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * This indicates whether the Charging Station is able to perform the availability change.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("status")
+    public ChangeAvailabilityStatusEnumType status;
+    /**
+     * Element providing more information about the status.
+     *
+     *
+     */
+    @JsonProperty("statusInfo")
+    public StatusInfoType statusInfo;
+
+    public ChangeAvailabilityConfirmation(ChangeAvailabilityStatusEnumType status) {
+        requiredValidator.validate(status);
+        this.status = status;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setStatus(ChangeAvailabilityStatusEnumType status) {
+        requiredValidator.validate(status);
+        this.status = status;
+    }
+
+    public void setStatusInfo(StatusInfoType statusInfo) {
+        this.statusInfo = statusInfo;
+    }
+
+    @Override
+    public boolean validate() {
+        return requiredValidator.safeValidate(status);
+    }
+}
