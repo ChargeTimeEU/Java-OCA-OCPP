4c4c45056d2de753d746529f92a019269fecd30b
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-16 17:38:06 +0200

    added ChangeAvailabilityStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/ChangeAvailabilityStatusEnumType.java
@@ -0,0 +1,42 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * This indicates whether the Charging Station is able to perform the availability change.
+ *
+ *
+ */
+public enum ChangeAvailabilityStatusEnumType {
+    ACCEPTED("Accepted"),
+    REJECTED("Rejected"),
+    SCHEDULED("Scheduled");
+    private final String value;
+
+    ChangeAvailabilityStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static ChangeAvailabilityStatusEnumType fromValue(String value) {
+        return findByField(
+                ChangeAvailabilityStatusEnumType.class,
+                ChangeAvailabilityStatusEnumType::value,
+                value
+        );
+    }
+}
