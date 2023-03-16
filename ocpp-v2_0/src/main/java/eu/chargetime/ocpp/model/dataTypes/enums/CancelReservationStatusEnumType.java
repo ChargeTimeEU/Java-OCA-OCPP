28a753d18d5dff590218675bc7b4bcc382a4d45f
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-16 16:52:28 +0200

    added CancelReservationStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/CancelReservationStatusEnumType.java
@@ -0,0 +1,44 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.ToString;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * This indicates the success or failure of the canceling of a reservation by CSMS.
+ *
+ *
+ */
+public enum CancelReservationStatusEnumType {
+
+    ACCEPTED("Accepted"),
+    REJECTED("Rejected");
+    private final String value;
+
+    CancelReservationStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static CancelReservationStatusEnumType fromValue(String value) {
+        return findByField(
+                CancelReservationStatusEnumType.class,
+                CancelReservationStatusEnumType::value,
+                value
+        );
+    }
+
+}
