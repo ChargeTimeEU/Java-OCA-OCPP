5734a5257ccafbcb02e718ac5efb92b8965c4cfb
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-17 12:22:38 +0200

    added ClearDisplayMessageResponse.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/ClearDisplayMessageResponse.java
@@ -0,0 +1,72 @@
+package eu.chargetime.ocpp.model.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.Confirmation;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.dataTypes.StatusInfoType;
+import eu.chargetime.ocpp.model.dataTypes.enums.ClearMessageStatusEnumType;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "status",
+        "statusInfo"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class ClearDisplayMessageResponse extends Confirmation {
+
+    private transient RequiredValidator requiredValidator = new RequiredValidator();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * Returns whether the Charging Station has been able to remove the message.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("status")
+    public ClearMessageStatusEnumType status;
+    /**
+     * Element providing more information about the status.
+     *
+     *
+     */
+    @JsonProperty("statusInfo")
+    public StatusInfoType statusInfo;
+
+    public ClearDisplayMessageResponse(ClearMessageStatusEnumType status) {
+        requiredValidator.validate(status);
+        this.status = status;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setStatus(ClearMessageStatusEnumType status) {
+        requiredValidator.validate(status);
+        this.status = status;
+    }
+
+    public void setStatusInfo(StatusInfoType statusInfo) {
+        this.statusInfo = statusInfo;
+    }
+
+    @Override
+    public boolean validate() {
+        return requiredValidator.safeValidate(status);
+    }
+}
