526e3dd8104cb296db7f6dbc960760cada537ac7
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-20 12:07:01 +0200

    added CustomerInformationStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/CustomerInformationStatusEnumType.java
@@ -0,0 +1,43 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * Indicates whether the request was accepted.
+ *
+ *
+ */
+public enum CustomerInformationStatusEnumType {
+
+    ACCEPTED("Accepted"),
+    REJECTED("Rejected"),
+    INVALID("Invalid");
+    private final String value;
+
+    CustomerInformationStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static CustomerInformationStatusEnumType fromValue(String value) {
+        return findByField(
+                CustomerInformationStatusEnumType.class,
+                CustomerInformationStatusEnumType::value,
+                value
+        );
+    }
+}
