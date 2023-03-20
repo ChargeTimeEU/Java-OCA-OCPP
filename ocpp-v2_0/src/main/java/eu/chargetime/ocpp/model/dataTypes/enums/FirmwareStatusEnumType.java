623071f30a3d0c067d90f0c904f23ee6dfa8d1e5
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-20 14:14:59 +0200

    added FirmwareStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/FirmwareStatusEnumType.java
@@ -0,0 +1,54 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * This contains the progress status of the firmware installation.
+ *
+ *
+ */
+public enum FirmwareStatusEnumType {
+
+    DOWNLOADED("Downloaded"),
+    DOWNLOAD_FAILED("DownloadFailed"),
+    DOWNLOADING("Downloading"),
+    DOWNLOAD_SCHEDULED("DownloadScheduled"),
+    DOWNLOAD_PAUSED("DownloadPaused"),
+    IDLE("Idle"),
+    INSTALLATION_FAILED("InstallationFailed"),
+    INSTALLING("Installing"),
+    INSTALLED("Installed"),
+    INSTALL_REBOOTING("InstallRebooting"),
+    INSTALL_SCHEDULED("InstallScheduled"),
+    INSTALL_VERIFICATION_FAILED("InstallVerificationFailed"),
+    INVALID_SIGNATURE("InvalidSignature"),
+    SIGNATURE_VERIFIED("SignatureVerified");
+    private final String value;
+
+    FirmwareStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static FirmwareStatusEnumType fromValue(String value) {
+        return findByField(
+                FirmwareStatusEnumType.class,
+                FirmwareStatusEnumType::value,
+                value
+        );
+    }
+}
