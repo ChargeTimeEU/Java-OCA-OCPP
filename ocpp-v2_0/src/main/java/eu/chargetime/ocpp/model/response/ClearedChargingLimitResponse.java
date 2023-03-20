bc5f3fe38eb97e8db8d11789a2baf71d732d8000
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-20 11:37:43 +0200

    added 'customData' fields for empty requests\response + added some config annotation

--- ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/ClearedChargingLimitResponse.java
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/ClearedChargingLimitResponse.java
@@ -1,8 +1,29 @@
 package eu.chargetime.ocpp.model.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import eu.chargetime.ocpp.model.Confirmation;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
 public class ClearedChargingLimitResponse extends Confirmation {
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+
     @Override
     public boolean validate() {
         return true;
