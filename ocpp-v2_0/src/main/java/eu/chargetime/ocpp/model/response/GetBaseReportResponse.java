ca2ed6055d74fbc498eead1bfb9634165d24a622
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-20 16:53:13 +0200

    added missed fields + validation

--- ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/GetBaseReportResponse.java
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/GetBaseReportResponse.java
@@ -3,6 +3,9 @@ package eu.chargetime.ocpp.model.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.chargetime.ocpp.model.Confirmation;
 import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.dataTypes.StatusInfoType;
+import eu.chargetime.ocpp.model.dataTypes.enums.GenericDeviceModelStatusEnumType;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -12,6 +15,8 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = false)
 public class GetBaseReportResponse extends Confirmation {
 
+    private transient RequiredValidator requiredValidator = new RequiredValidator();
+
     /**
      * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
      *
@@ -19,9 +24,43 @@ public class GetBaseReportResponse extends Confirmation {
     @JsonProperty("customData")
     public CustomDataType customData;
 
+    /**
+     * This indicates whether the Charging Station is able to accept this request.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("status")
+    public GenericDeviceModelStatusEnumType status;
+    /**
+     * Element providing more information about the status.
+     *
+     *
+     */
+    @JsonProperty("statusInfo")
+    public StatusInfoType statusInfo;
+
+    public GetBaseReportResponse(GenericDeviceModelStatusEnumType status) {
+        requiredValidator.validate(status);
+        this.status = status;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setStatus(GenericDeviceModelStatusEnumType status) {
+        requiredValidator.validate(status);
+        this.status = status;
+    }
+
+    public void setStatusInfo(StatusInfoType statusInfo) {
+        this.statusInfo = statusInfo;
+    }
+
     @Override
     public boolean validate() {
-        return true;
+        return requiredValidator.safeValidate(status);
     }
 
 }
