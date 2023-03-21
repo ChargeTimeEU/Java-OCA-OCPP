2bc11a7b5180e8c2458f2b1874e3a8f319273aec
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-21 13:43:03 +0200

    added GetInstalledCertificateStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/GetInstalledCertificateStatusEnumType.java
@@ -0,0 +1,42 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * Charging Station indicates if it can process the request.
+ *
+ *
+ */
+public enum GetInstalledCertificateStatusEnumType {
+
+    ACCEPTED("Accepted"),
+    NOT_FOUND("NotFound");
+    private final String value;
+
+    GetInstalledCertificateStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static GetInstalledCertificateStatusEnumType fromValue(String value) {
+        return findByField(
+                GetInstalledCertificateStatusEnumType.class,
+                GetInstalledCertificateStatusEnumType::value,
+                value
+        );
+    }
+}
