7c6cb215ae9cf6594857b421188665f33db0c66f
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-21 14:06:54 +0200

    added GetLocalListVersionResponse.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/GetLocalListVersionResponse.java
@@ -0,0 +1,59 @@
+package eu.chargetime.ocpp.model.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.Confirmation;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
+import eu.chargetime.ocpp.model.validation.Validator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "versionNumber"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class GetLocalListVersionResponse extends Confirmation {
+
+    private transient Validator<Object> requiredValidator = new RequiredValidator();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * This contains the current version number of the local authorization list in the Charging Station.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("versionNumber")
+    public Integer versionNumber;
+
+    public GetLocalListVersionResponse(Integer versionNumber) {
+        requiredValidator.validate(versionNumber);
+        this.versionNumber = versionNumber;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setVersionNumber(Integer versionNumber) {
+        requiredValidator.validate(versionNumber);
+        this.versionNumber = versionNumber;
+    }
+
+    @Override
+    public boolean validate() {
+        return requiredValidator.safeValidate(versionNumber);
+    }
+}
