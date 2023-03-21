40dbc95dfb097832a502cd49863570466c9c4d0d
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-21 14:55:44 +0200

    added HeartbeatRequest.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/request/HeartbeatRequest.java
@@ -0,0 +1,35 @@
+package eu.chargetime.ocpp.model.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import eu.chargetime.ocpp.model.RequestWithId;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class HeartbeatRequest extends RequestWithId {
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    @JsonPropertyDescription("This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.")
+    public CustomDataType customData;
+
+    @Override
+    public boolean transactionRelated() {
+        return false;
+    }
+
+    @Override
+    public boolean validate() {
+        return true;
+    }
+}
