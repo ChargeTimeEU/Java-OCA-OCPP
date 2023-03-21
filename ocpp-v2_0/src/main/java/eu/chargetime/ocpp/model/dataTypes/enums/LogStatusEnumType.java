8ec79b874d746ec3b87b6e13a5d6cf1ba12f9fef
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-21 14:19:44 +0200

    added GLogStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/LogStatusEnumType.java
@@ -0,0 +1,43 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * This field indicates whether the Charging Station was able to accept the request.
+ *
+ *
+ */
+public enum LogStatusEnumType {
+
+    ACCEPTED("Accepted"),
+    REJECTED("Rejected"),
+    ACCEPTED_CANCELED("AcceptedCanceled");
+    private final String value;
+
+    LogStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static LogStatusEnumType fromValue(String value) {
+        return findByField(
+                LogStatusEnumType.class,
+                LogStatusEnumType::value,
+                value
+        );
+    }
+}
