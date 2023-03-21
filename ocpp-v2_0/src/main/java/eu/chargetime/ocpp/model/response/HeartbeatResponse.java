7adae170e74314bb6e1540f109224d09827ae7a1
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-21 14:59:46 +0200

    added HeartbeatResponse.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/HeartbeatResponse.java
@@ -0,0 +1,61 @@
+package eu.chargetime.ocpp.model.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.Confirmation;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
+import eu.chargetime.ocpp.model.validation.Validator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "currentTime"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class HeartbeatResponse extends Confirmation {
+
+    private transient Validator<Object> requiredValidator = new RequiredValidator();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * Contains the current time of the CSMS.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("currentTime")
+    public Date currentTime;
+
+    public HeartbeatResponse(Date currentTime) {
+        requiredValidator.validate(currentTime);
+        this.currentTime = currentTime;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setCurrentTime(Date currentTime) {
+        requiredValidator.validate(currentTime);
+        this.currentTime = currentTime;
+    }
+
+    @Override
+    public boolean validate() {
+        return requiredValidator.safeValidate(currentTime);
+    }
+}
