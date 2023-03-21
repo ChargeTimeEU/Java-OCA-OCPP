7b8deb10d94afe9d92c4a75cb0c68ffe05e1f37b
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-21 13:31:04 +0200

    added GetDisplayMessagesRequest.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/request/GetDisplayMessagesRequest.java
@@ -0,0 +1,105 @@
+package eu.chargetime.ocpp.model.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.RequestWithId;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.dataTypes.enums.MessagePriorityEnumType;
+import eu.chargetime.ocpp.model.dataTypes.enums.MessageStateEnumType;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
+import eu.chargetime.ocpp.model.validation.Validator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "id",
+        "requestId",
+        "priority",
+        "state"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class GetDisplayMessagesRequest extends RequestWithId {
+
+    private transient Validator<Object> requiredValidator = new RequiredValidator();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * If provided the Charging Station shall return Display Messages of the given ids. This field SHALL NOT contain more ids than set in &lt;<configkey-number-of-display-messages,NumberOfDisplayMessages.maxLimit>>
+     *
+     *
+     *
+     */
+    @JsonProperty("id")
+    public List<Integer> id;
+    /**
+     * The Id of this request.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("requestId")
+    public Integer requestId;
+    /**
+     * If provided the Charging Station shall return Display Messages with the given priority only.
+     *
+     *
+     */
+    @JsonProperty("priority")
+    public MessagePriorityEnumType priority;
+    /**
+     * If provided the Charging Station shall return Display Messages with the given state only.
+     *
+     *
+     */
+    @JsonProperty("state")
+    public MessageStateEnumType state;
+
+    public GetDisplayMessagesRequest(Integer requestId) {
+        requiredValidator.validate(requestId);
+        this.requestId = requestId;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setId(List<Integer> id) {
+        this.id = id;
+    }
+
+    public void setRequestId(Integer requestId) {
+        requiredValidator.validate(requestId);
+        this.requestId = requestId;
+    }
+
+    public void setPriority(MessagePriorityEnumType priority) {
+        this.priority = priority;
+    }
+
+    public void setState(MessageStateEnumType state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean transactionRelated() {
+        return false;
+    }
+
+    @Override
+    public boolean validate() {
+        return requiredValidator.safeValidate(requestId);
+    }
+}
