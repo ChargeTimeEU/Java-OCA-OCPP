313a5f70928e7d602df9a0017679797c7dbf8669
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-21 13:28:29 +0200

    added MessagePriorityEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/MessagePriorityEnumType.java
@@ -0,0 +1,43 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * If provided the Charging Station shall return Display Messages with the given priority only.
+ *
+ *
+ */
+public enum MessagePriorityEnumType {
+
+    ALWAYS_FRONT("AlwaysFront"),
+    IN_FRONT("InFront"),
+    NORMAL_CYCLE("NormalCycle");
+    private final String value;
+
+    MessagePriorityEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static MessagePriorityEnumType fromValue(String value) {
+        return findByField(
+                MessagePriorityEnumType.class,
+                MessagePriorityEnumType::value,
+                value
+        );
+    }
+}
