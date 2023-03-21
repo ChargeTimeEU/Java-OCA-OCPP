ff90d2f4042b060932b9ccea5cf5cbcbbb894c1b
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-21 14:03:45 +0200

    added GetLocalListVersionRequest.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/request/GetLocalListVersionRequest.java
@@ -0,0 +1,41 @@
+package eu.chargetime.ocpp.model.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.RequestWithId;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData"
+})
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class GetLocalListVersionRequest extends RequestWithId {
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    @JsonPropertyDescription("This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.")
+    public CustomDataType customData;
+
+    @Override
+    public boolean transactionRelated() {
+        return false;
+    }
+
+    @Override
+    public boolean validate() {
+        return true;
+    }
+}
