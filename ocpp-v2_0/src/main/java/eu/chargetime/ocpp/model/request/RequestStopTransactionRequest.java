1e9f7311aa67c5ffd5ee9fc9399d3a2bce3d94f4
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-25 21:00:12 +0200

    added RequestStopTransactionRequest.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/request/RequestStopTransactionRequest.java
@@ -0,0 +1,70 @@
+package eu.chargetime.ocpp.model.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.RequestWithId;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.validation.OCPP2PrimDatatypes;
+import eu.chargetime.ocpp.model.validation.Validator;
+import eu.chargetime.ocpp.model.validation.ValidatorBuilder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "transactionId"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class RequestStopTransactionRequest extends RequestWithId {
+
+    private transient Validator transactionIdValidator =
+            new ValidatorBuilder()
+                    .addRule(OCPP2PrimDatatypes.identifierString())
+                    .addRule(OCPP2PrimDatatypes.string36())
+                    .setRequired(true)
+                    .build();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * The identifier of the transaction which the Charging Station is requested to stop.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("transactionId")
+    public String transactionId;
+
+    public RequestStopTransactionRequest(String transactionId) {
+        transactionIdValidator.validate(transactionId);
+        this.transactionId = transactionId;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setTransactionId(String transactionId) {
+        transactionIdValidator.validate(transactionId);
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public boolean transactionRelated() {
+        return false;
+    }
+
+    @Override
+    public boolean validate() {
+        return transactionIdValidator.safeValidate(transactionId);
+    }
+}
