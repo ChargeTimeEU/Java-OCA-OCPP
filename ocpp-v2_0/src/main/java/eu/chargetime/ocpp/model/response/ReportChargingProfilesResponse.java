5ada87012a23d0aed1ecf7e904888e50d5689c3e
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-25 20:47:05 +0200

    added ReportChargingProfilesResponse.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/ReportChargingProfilesResponse.java
@@ -0,0 +1,27 @@
+package eu.chargetime.ocpp.model.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import eu.chargetime.ocpp.model.Confirmation;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class ReportChargingProfilesResponse extends Confirmation {
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+
+    @Override
+    public boolean validate() {
+        return true;
+    }
+}
