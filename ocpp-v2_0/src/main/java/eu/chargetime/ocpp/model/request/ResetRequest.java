3e5b127c1a7b32e9281b272f805e7003d6fb9965
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-25 21:20:25 +0200

    added ResetRequest.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/request/ResetRequest.java
@@ -0,0 +1,77 @@
+package eu.chargetime.ocpp.model.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.RequestWithId;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.dataTypes.enums.ResetEnumType;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
+import eu.chargetime.ocpp.model.validation.Validator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "type",
+        "evseId"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class ResetRequest extends RequestWithId {
+
+    private transient Validator<Object> requiredValidator = new RequiredValidator();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * This contains the type of reset that the Charging Station or EVSE should perform.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("type")
+    public ResetEnumType type;
+    /**
+     * This contains the ID of a specific EVSE that needs to be reset, instead of the entire Charging Station.
+     *
+     *
+     */
+    @JsonProperty("evseId")
+    public Integer evseId;
+
+    public ResetRequest(ResetEnumType type) {
+        requiredValidator.validate(type);
+        this.type = type;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setType(ResetEnumType type) {
+        requiredValidator.validate(type);
+        this.type = type;
+    }
+
+    public void setEvseId(Integer evseId) {
+        this.evseId = evseId;
+    }
+
+    @Override
+    public boolean transactionRelated() {
+        return false;
+    }
+
+    @Override
+    public boolean validate() {
+        return false;
+    }
+}
