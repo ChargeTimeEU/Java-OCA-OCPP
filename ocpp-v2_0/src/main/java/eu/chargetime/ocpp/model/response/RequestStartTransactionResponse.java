a754ce089f541b9d19822841ca14302fd5309ff2
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-25 20:56:19 +0200

    added RequestStartTransactionResponse.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/response/RequestStartTransactionResponse.java
@@ -0,0 +1,97 @@
+package eu.chargetime.ocpp.model.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.Confirmation;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.dataTypes.StatusInfoType;
+import eu.chargetime.ocpp.model.dataTypes.enums.RequestStartStopStatusEnumType;
+import eu.chargetime.ocpp.model.validation.OCPP2PrimDatatypes;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
+import eu.chargetime.ocpp.model.validation.Validator;
+import eu.chargetime.ocpp.model.validation.ValidatorBuilder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "status",
+        "statusInfo",
+        "transactionId"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class RequestStartTransactionResponse extends Confirmation {
+
+    private transient Validator<Object> requiredValidator = new RequiredValidator();
+
+    private transient Validator transactionIdValidator =
+            new ValidatorBuilder()
+                    .addRule(OCPP2PrimDatatypes.identifierString())
+                    .addRule(OCPP2PrimDatatypes.string36())
+                    .build();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * Status indicating whether the Charging Station accepts the request to start a transaction.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("status")
+    public RequestStartStopStatusEnumType status;
+    /**
+     * Element providing more information about the status.
+     *
+     *
+     */
+    @JsonProperty("statusInfo")
+    public StatusInfoType statusInfo;
+    /**
+     * When the transaction was already started by the Charging Station before the RequestStartTransactionRequest was received, for example: cable plugged in first. This contains the transactionId of the already started transaction.
+     *
+     *
+     */
+    @JsonProperty("transactionId")
+    public String transactionId;
+
+    public RequestStartTransactionResponse(RequestStartStopStatusEnumType status) {
+        requiredValidator.validate(status);
+        this.status = status;
+    }
+
+    public void setTransactionIdValidator(Validator transactionIdValidator) {
+        this.transactionIdValidator = transactionIdValidator;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setStatus(RequestStartStopStatusEnumType status) {
+        this.status = status;
+    }
+
+    public void setStatusInfo(StatusInfoType statusInfo) {
+        this.statusInfo = statusInfo;
+    }
+
+    public void setTransactionId(String transactionId) {
+        transactionIdValidator.validate(transactionId);
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public boolean validate() {
+        return requiredValidator.safeValidate(status);
+    }
+}
