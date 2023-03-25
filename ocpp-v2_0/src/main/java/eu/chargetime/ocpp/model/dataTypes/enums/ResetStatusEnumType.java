41fa50e5fe4f2c581eeb45191a827b19da35ccba
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-25 21:22:14 +0200

    added ResetStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/ResetStatusEnumType.java
@@ -0,0 +1,43 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * This indicates whether the Charging Station is able to perform the reset.
+ *
+ *
+ */
+public enum ResetStatusEnumType {
+
+    ACCEPTED("Accepted"),
+    REJECTED("Rejected"),
+    SCHEDULED("Scheduled");
+    private final String value;
+
+    ResetStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static ResetStatusEnumType fromValue(String value) {
+        return findByField(
+                ResetStatusEnumType.class,
+                ResetStatusEnumType::value,
+                value
+        );
+    }
+}
