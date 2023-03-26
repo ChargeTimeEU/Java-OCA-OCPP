4d489009e7a90d84cbaa328c4409202c9ab0e7d2
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-26 22:33:05 +0300

    added SetMonitoringStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/SetMonitoringStatusEnumType.java
@@ -0,0 +1,46 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * Status is OK if a value could be returned. Otherwise this will indicate the reason why a value could not be returned.
+ *
+ *
+ */
+public enum SetMonitoringStatusEnumType {
+
+    ACCEPTED("Accepted"),
+    UNKNOWN_COMPONENT("UnknownComponent"),
+    UNKNOWN_VARIABLE("UnknownVariable"),
+    UNSUPPORTED_MONITOR_TYPE("UnsupportedMonitorType"),
+    REJECTED("Rejected"),
+    DUPLICATE("Duplicate");
+    private final String value;
+
+    SetMonitoringStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static SetMonitoringStatusEnumType fromValue(String value) {
+        return findByField(
+                SetMonitoringStatusEnumType.class,
+                SetMonitoringStatusEnumType::value,
+                value
+        );
+    }
+}
