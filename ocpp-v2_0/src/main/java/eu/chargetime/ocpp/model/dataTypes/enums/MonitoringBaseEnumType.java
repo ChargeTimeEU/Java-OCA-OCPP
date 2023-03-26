f32ee1fcb3e51f72b2f53a42c66fb23259c8d172
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-26 21:23:17 +0300

    added MonitoringBaseEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/MonitoringBaseEnumType.java
@@ -0,0 +1,43 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * Specify which monitoring base will be set
+ *
+ *
+ */
+public enum MonitoringBaseEnumType {
+
+    ALL("All"),
+    FACTORY_DEFAULT("FactoryDefault"),
+    HARD_WIRED_ONLY("HardWiredOnly");
+    private final String value;
+
+    MonitoringBaseEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static MonitoringBaseEnumType fromValue(String value) {
+        return findByField(
+                MonitoringBaseEnumType.class,
+                MonitoringBaseEnumType::value,
+                value
+        );
+    }
+}
