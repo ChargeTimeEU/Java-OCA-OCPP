953d7d04550dec95df8a74ebe2df9dcc4f694a14
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-26 21:53:01 +0300

    added OCPPTransportEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/OCPPTransportEnumType.java
@@ -0,0 +1,44 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * Communication_ Function. OCPP_ Transport. OCPP_ Transport_ Code
+ * urn:x-oca:ocpp:uid:1:569356
+ * Defines the transport protocol (e.g. SOAP or JSON). Note: SOAP is not supported in OCPP 2.0, but is supported by other versions of OCPP.
+ *
+ *
+ */
+public enum OCPPTransportEnumType {
+
+    JSON("JSON"),
+    SOAP("SOAP");
+    private final String value;
+
+    OCPPTransportEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static OCPPTransportEnumType fromValue(String value) {
+        return findByField(
+                OCPPTransportEnumType.class,
+                OCPPTransportEnumType::value,
+                value
+        );
+    }
+}
