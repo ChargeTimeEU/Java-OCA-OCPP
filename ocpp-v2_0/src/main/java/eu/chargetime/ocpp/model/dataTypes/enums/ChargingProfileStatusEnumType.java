ca7de6792df49f6a577b1e456c5d74854442ee9f
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-26 21:13:41 +0300

    added ChargingProfileStatusEnumType.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/dataTypes/enums/ChargingProfileStatusEnumType.java
@@ -0,0 +1,42 @@
+package eu.chargetime.ocpp.model.dataTypes.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+
+/**
+ * Returns whether the Charging Station has been able to process the message successfully. This does not guarantee the schedule will be followed to the letter. There might be other constraints the Charging Station may need to take into account.
+ *
+ *
+ */
+public enum ChargingProfileStatusEnumType {
+
+    ACCEPTED("Accepted"),
+    REJECTED("Rejected");
+    private final String value;
+
+    ChargingProfileStatusEnumType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static ChargingProfileStatusEnumType fromValue(String value) {
+        return findByField(
+                ChargingProfileStatusEnumType.class,
+                ChargingProfileStatusEnumType::value,
+                value
+        );
+    }
+}
