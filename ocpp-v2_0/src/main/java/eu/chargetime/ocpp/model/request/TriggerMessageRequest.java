1471edd0d61e60c0d0faf04fe0ba8d6f9d8ee9d3
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-27 12:18:05 +0300

    added TriggerMessageRequest.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/model/request/TriggerMessageRequest.java
@@ -0,0 +1,80 @@
+package eu.chargetime.ocpp.model.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import eu.chargetime.ocpp.model.RequestWithId;
+import eu.chargetime.ocpp.model.dataTypes.CustomDataType;
+import eu.chargetime.ocpp.model.dataTypes.EVSEType;
+import eu.chargetime.ocpp.model.dataTypes.enums.MessageTriggerEnumType;
+import eu.chargetime.ocpp.model.validation.RequiredValidator;
+import eu.chargetime.ocpp.model.validation.Validator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "evse",
+        "requestedMessage"
+})
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class TriggerMessageRequest extends RequestWithId {
+
+    private transient Validator<Object> requiredValidator = new RequiredValidator();
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomDataType customData;
+    /**
+     * EVSE
+     * urn:x-oca:ocpp:uid:2:233123
+     * Electric Vehicle Supply Equipment
+     *
+     *
+     */
+    @JsonProperty("evse")
+    public EVSEType evse;
+    /**
+     * Type of message to be triggered.
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("requestedMessage")
+    public MessageTriggerEnumType requestedMessage;
+
+    public TriggerMessageRequest(MessageTriggerEnumType requestedMessage) {
+        requiredValidator.validate(requestedMessage);
+        this.requestedMessage = requestedMessage;
+    }
+
+    public void setCustomData(CustomDataType customData) {
+        this.customData = customData;
+    }
+
+    public void setEvse(EVSEType evse) {
+        this.evse = evse;
+    }
+
+    public void setRequestedMessage(MessageTriggerEnumType requestedMessage) {
+        requiredValidator.validate(requestedMessage);
+        this.requestedMessage = requestedMessage;
+    }
+
+    @Override
+    public boolean transactionRelated() {
+        return false;
+    }
+
+    @Override
+    public boolean validate() {
+        return requiredValidator.safeValidate(requestedMessage);
+    }
+}
