02e42f27b40db3835f4cdf1ac9cb31beca285577
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-29 15:55:33 +0300

    added IClientClearCacheRequestHandler.java

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/features/basic/handlers/client/IClientClearCacheRequestHandler.java
@@ -0,0 +1,9 @@
+package eu.chargetime.ocpp.features.basic.handlers.client;
+
+import eu.chargetime.ocpp.model.request.ClearCacheRequest;
+import eu.chargetime.ocpp.model.response.ClearCacheResponse;
+
+/** Charging Station handler of {@link ClearCacheRequest} */
+public interface IClientClearCacheRequestHandler {
+    ClearCacheResponse handleGetVariablesRequest(ClearCacheRequest request);
+}
