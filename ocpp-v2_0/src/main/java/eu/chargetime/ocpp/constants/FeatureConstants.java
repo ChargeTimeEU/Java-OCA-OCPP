dd06d74e93ab3e792ccd41952ea6bffdb50df9ea
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-03-29 13:25:19 +0300

    added new enum for constants

---
+++ ocpp-v2_0/src/main/java/eu/chargetime/ocpp/constants/FeatureConstants.java
@@ -0,0 +1,39 @@
+package eu.chargetime.ocpp.constants;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static eu.chargetime.ocpp.util.EnumUtil.findByField;
+
+public enum FeatureConstants {
+
+    BOOT_NOTIFICATION("BootNotification"),
+    GET_VARIABLES("GetVariables"),
+    SET_VARIABLES("SetVariables"),
+    STATUS_NOTIFICATION("StatusNotification");
+
+    private final String value;
+
+    FeatureConstants(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static FeatureConstants fromValue(String value) {
+        return findByField(
+                FeatureConstants.class,
+                FeatureConstants::value,
+                value
+        );
+    }
+}
