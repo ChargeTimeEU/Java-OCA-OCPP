606023768ba61aaaace0d0eed65fb95ac2ff835a
Johannes Zangl <johannes.zangl@smartmechatronics.de>
2024-01-25 11:51:37 +0100

    Allow configuration of allowed password string length
    This change allows the configuration of the password length with entries in JSONConfiguration.

--- ocpp-v1_6/src/main/java/eu/chargetime/ocpp/JSONServer.java
+++ ocpp-v1_6/src/main/java/eu/chargetime/ocpp/JSONServer.java
@@ -69,7 +69,7 @@ public class JSONServer implements IServerAPI {
     protocols.add(new Protocol(""));
     draftOcppOnly = new Draft_6455(Collections.emptyList(), protocols);
 
-    if(configuration.getParameter("HTTP_HEALTH_CHECK_ENABLED", true)) {
+    if(configuration.getParameter(JSONConfiguration.HTTP_HEALTH_CHECK_ENABLED, true)) {
       logger.info("JSONServer 1.6 with HttpHealthCheckDraft");
       this.listener = new WebSocketListener(sessionFactory, configuration, draftOcppOnly, new Draft_HttpHealthCheck());
     } else {
